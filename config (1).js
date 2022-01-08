6f761054b7038591388dd9b5d7304809dd7e0483
WELLTAG <Welltagytb@gmail.com>
2022-01-08 10:43:03 +0200

    Add files via upload

---
+++ config (1).js
@@ -0,0 +1,21 @@
+module.exports = {
+    app: {
+        prefix: '?',
+        token: 'OTI4NjIyMjQ5ODY5NzA5Mzcy.Ydbc3Q._q0JNLrTteFXStmGLSELVTUatEM',
+        playing: '?help | 💗 Par Stasair' 
+      
+        
+    },
+
+    opt: {
+        
+        maxVol: 300,
+        loopMessage: false,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
