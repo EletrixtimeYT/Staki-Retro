89874dcf6dc036574c331293438f0d38262ae988
WELLTAG <Welltagytb@gmail.com>
2022-01-08 10:53:53 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
-    console.log(` ${client.user.username} est connecté\n-> Dans ${client.guilds.cache.size} serveurs avec un total de  ${client.users.cache.size} membres `);
+    console.log(` ${client.user.username} est connecté\n-> Dans ${client.guilds.cache.size} serveurs  `);
 
     client.user.setActivity(client.config.app.playing);
 };
